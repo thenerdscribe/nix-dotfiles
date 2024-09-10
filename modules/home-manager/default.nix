7749ffcbcdf092aff54adcab963c5088e49922d8
Ryan Morton <morton.ryan92@gmail.com>
2024-09-09 21:14:30 -0700

    Fixed bat theme

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -69,7 +69,18 @@
     bat = {
       enable = true;
       config = {
-        theme = "Catppuccin"; # Fix this for bat
+        theme = "Catppuccin Mocha"; # Fix this for bat
+      };
+      themes = {
+        "Catppuccin Mocha" = {
+          src = pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "bat";
+            rev = "d3feec47b16a8e99eabb34cdfbaa115541d374fc";
+            sha256 = "sha256-s0CHTihXlBMCKmbBBb8dUhfgOOQu9PBCQ+uviy7o47w=";
+          };
+          file = "themes/Catppuccin Mocha.tmTheme";
+        };
       };
     };
     neovim = {
