a61698b3ca0ae99043e0acb95a026b598b898e5f
Ryan Morton <morton.ryan92@gmail.com>
2024-09-12 19:47:07 -0700

    Add MBA config

--- flake.nix
+++ flake.nix
@@ -23,6 +23,65 @@
       # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
       # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
+      darwinConfigurations."Ryans-MacBook-Air" = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        pkgs = import nixpkgs {
+          system = "aarch64-darwin";
+          config = {
+            allowUnfree = true;
+            allowUnfreePredicate = _: true;
+          };
+        };
+        modules = [
+          (
+            { pkgs, ... }:
+            {
+              users.users.ryanmorton = {
+                uid = 501;
+                name = "ryanmorton";
+                home = "/Users/ryanmorton";
+                shell = pkgs.zsh;
+              };
+              programs.zsh.enable = true;
+              # homebrew.enable = true;
+              # homebrew.brews = [ ];
+              environment = {
+                shells = [
+                  pkgs.bash
+                  pkgs.zsh
+                ];
+                loginShell = pkgs.zsh;
+                systemPackages = [
+                  pkgs.coreutils
+                  pkgs.git
+                  pkgs.curl
+                  pkgs.wget
+                  pkgs.zellij
+                  pkgs.jankyborders
+                ];
+              };
+              nix.extraOptions = ''
+                experimental-features = nix-command flakes
+              '';
+              services.nix-daemon.enable = true;
+              system.stateVersion = 4;
+              system.defaults.NSGlobalDomain._HIHideMenuBar = true;
+              system.defaults.dock.autohide = true;
+            }
+          )
+          home-manager.darwinModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.ryanmorton.imports = [
+                ./modules/home-manager
+              ];
+            };
+          }
+        ];
+      };
+
       darwinConfigurations."Ryans-Mac-Mini" = darwin.lib.darwinSystem {
         system = "x86_64-darwin";
         pkgs = import nixpkgs {
