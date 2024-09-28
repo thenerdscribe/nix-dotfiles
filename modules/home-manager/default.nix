14e881e1dbe898a2bb011c11679277c346b61914
Ryan Morton <morton.ryan92@gmail.com>
2024-09-28 09:15:31 -0700

    Further customization

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -32,11 +32,13 @@
     nix-prefetch-git
     cachix
     magic-wormhole
+    ice-bar
   ];
   home.sessionVariables = {
     EDITOR = "nvim";
     FZF_DEFAULT_COMMAND = "fd --hidden --strip-cwd-prefix --exclude .git";
     FZF_DEFAULT_OPTS = ''
+        --height=40% \
       --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
       --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
       --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8'';
@@ -231,6 +233,7 @@
         gcm = "git checkout master";
         php = "herd php";
         composer = "herd composer";
+        za = "zellij attach $(zellij list-sessions --no-formatting --short | fzf)";
       };
       initExtra = ''
                 setopt autopushd
