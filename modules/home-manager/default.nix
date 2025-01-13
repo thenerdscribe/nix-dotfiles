a9fcedee2d7db1b56cbc44373ecb34800347f1f6
Ryan Morton <morton.ryan92@gmail.com>
2025-01-13 09:41:06 -0700

    Get rid of commented out options

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -45,13 +45,7 @@
   ];
   home.sessionVariables = {
     EDITOR = "nvim";
-    # LIBSQLITE = "${pkgs.sqlite.out}/lib/libsqlite3.dylib";
     FZF_DEFAULT_COMMAND = "fd --hidden --strip-cwd-prefix --exclude .git";
-    FZF_DEFAULT_OPTS = ''
-        --height=20% \
-      --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
-      --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-      --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8'';
     FZF_CTRL_T_OPTS = ''
       --walker-skip .git,node_modules,target \
       --preview 'bat -n --color=always {}' \
