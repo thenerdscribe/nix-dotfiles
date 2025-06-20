214a5cb05d825f3a4c295430dab501a14b5deaab
Ryan Morton <morton.ryan92@gmail.com>
2025-06-20 09:30:24 -0700

    Test

--- flake.nix
+++ flake.nix
@@ -1,188 +1,197 @@
 {
-    description = "ryanmorton system management";
+  description = "ryanmorton system management";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-        zen-browser = {
-    url = "github:0xc000022070/zen-browser-flake";
-    # IMPORTANT: we're using "libgbm" and is only available in unstable so ensure
-    # to have it up-to-date or simply don't specify the nixpkgs input  
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      # IMPORTANT: we're using "libgbm" and is only available in unstable so ensure
+      # to have it up-to-date or simply don't specify the nixpkgs input
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     hypr-darkwindow = {
       url = "github:alexhulbert/Hyprchroma";
       inputs.hyprland.follows = "nixpkgs";
     };
-          hyprshell.url = "github:H3rmt/hyprswitch?ref=hyprshell";
+    hyprshell.url = "github:H3rmt/hyprswitch?ref=hyprshell";
     hyprshell.inputs.nixpkgs.follows = "nixpkgs";
-        home-manager = {
-            url = "github:nix-community/home-manager/master";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-        darwin.url = "github:lnl7/nix-darwin";
-        darwin.inputs.nixpkgs.follows = "nixpkgs";
-        ghostty = {
-            url = "github:ghostty-org/ghostty";
-        };
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    ghostty = {
+      url = "github:ghostty-org/ghostty";
+    };
+  };
 
-    outputs =
-        {
-        nixpkgs,
-        darwin,
-        home-manager,
-        ghostty,
-        hypr-darkwindow,
-        zen-browser,
-        ...
-        } @inputs:
-        {
-            darwinConfigurations.Ryans-MacBook-Air = darwin.lib.darwinSystem {
-                system = "aarch64-darwin";
-                pkgs = import nixpkgs {
-                    system = "aarch64-darwin";
-                    config = {
-                        allowUnfree = true;
-                        allowUnfreePredicate = _: true;
-                    };
-                };
-                modules = [
-                    (
-                        { pkgs, ... }:
-                        {
-                            users.users.ryanmorton = {
-                                uid = 501;
-                                name = "ryanmorton";
-                                home = "/Users/ryanmorton";
-                                shell = pkgs.zsh;
-                            };
-                            programs.zsh.enable = true;
-                            # homebrew.enable = true;
-                            # homebrew.brews = [ ];
-                            environment = {
-                                shells = [
-                                    pkgs.bash
-                                    pkgs.zsh
-                                ];
-                                systemPackages = [
-                                    pkgs.coreutils
-                                    pkgs.git
-                                    pkgs.curl
-                                    pkgs.wget
-                                    pkgs.zellij
-                                    pkgs.jankyborders
-                                    ghostty.packages.x86_64-darwin
-                                ];
-                            };
-                            nix.extraOptions = ''
-                experimental-features = nix-command flakes
-                            '';
-                            services.nix-daemon.enable = true;
-                            system.stateVersion = 4;
-                            system.defaults.NSGlobalDomain._HIHideMenuBar = false;
-                            system.defaults.dock.autohide = true;
-                        }
-                    )
-                    home-manager.darwinModules.home-manager
-                    {
-                        home-manager = {
-                            useGlobalPkgs = true;
-                            useUserPackages = true;
-                            users.ryanmorton.imports = [
-                                ./modules/home-manager
-                            ];
-                        };
-                    }
-                ];
-            };
-
-            nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-                system = "x86_64-linux";
-                specialArgs = { inherit inputs; };
-                pkgs = import nixpkgs {
-                    system = "x86_64-linux";
-                    config = {
-                        allowUnfree = true;
-                        allowUnfreePredicate = true;
-                        allowBroken = true;
-                    };
-                };
-                modules = [
-                    ./modules/nixos-config
-                    home-manager.nixosModules.home-manager
-                    {home-manager = {
-                        useGlobalPkgs = true;
-                        useUserPackages = true;
-                        backupFileExtension = "bak";
-
-                        users.ryanm.imports = [
-                            ./modules/home-manager
-                        ];
-                        extraSpecialArgs = { inherit inputs; system = "x86_64-linux";};
-                    };
-                    }
-                ];
-            };
-
-            darwinConfigurations.Ryans-Mac-Mini = darwin.lib.darwinSystem {
-                system = "x86_64-darwin";
-                pkgs = import nixpkgs {
-                    system = "x86_64-darwin";
-                    config = {
-                        allowUnfree = true;
-                        allowUnfreePredicate = true;
-                        allowBroken = true;
-                    };
-                };
-                modules = [
-                    (
-                        { pkgs, ... }:
-                        {
-                            users.users.ryanmorton = {
-                                uid = 501;
-                                name = "ryanmorton";
-                                home = "/Users/ryanmorton";
-                                shell = pkgs.zsh;
-                            };
-                            programs.zsh.enable = true;
-                            # homebrew.enable = true;
-                            # homebrew.brews = [ ];
-                            environment = {
-                                shells = [
-                                    pkgs.bash
-                                    pkgs.zsh
-                                ];
-                                systemPackages = [
-                                    pkgs.coreutils
-                                    pkgs.git
-                                    pkgs.curl
-                                    pkgs.wget
-                                    pkgs.zellij
-                                    pkgs.jankyborders
-                                ];
-                                systemPath = [
-                                    "~/.config/composer/vendor/bin"
-                                ];
-                            };
-                            nix.extraOptions = ''
-                experimental-features = nix-command flakes
-                            '';
-                            system.stateVersion = 4;
-                            system.defaults.NSGlobalDomain._HIHideMenuBar = false;
-                            system.defaults.dock.autohide = true;
-                        }
-                    )
-                    home-manager.darwinModules.home-manager
-                    {
-                        home-manager = {
-                            useGlobalPkgs = true;
-                            useUserPackages = true;
-                            users.ryanmorton.imports = [
-                                ./modules/home-manager
-                            ];
-                        };
-                    }
-                ];
-            };
+  outputs =
+    {
+      nixpkgs,
+      darwin,
+      home-manager,
+      ghostty,
+      hypr-darkwindow,
+      zen-browser,
+      ...
+    }@inputs:
+    {
+      darwinConfigurations.Ryans-MacBook-Air = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        pkgs = import nixpkgs {
+          system = "aarch64-darwin";
+          config = {
+            allowUnfree = true;
+            allowUnfreePredicate = _: true;
+          };
         };
+        modules = [
+          (
+            { pkgs, ... }:
+            {
+              users.users.ryanmorton = {
+                uid = 501;
+                name = "ryanmorton";
+                home = "/Users/ryanmorton";
+                shell = pkgs.zsh;
+              };
+              programs.zsh.enable = true;
+              # homebrew.enable = true;
+              # homebrew.brews = [ ];
+              environment = {
+                shells = [
+                  pkgs.bash
+                  pkgs.zsh
+                ];
+                systemPackages = [
+                  pkgs.coreutils
+                  pkgs.git
+                  pkgs.curl
+                  pkgs.wget
+                  pkgs.zellij
+                  pkgs.jankyborders
+                  ghostty.packages.x86_64-darwin
+                ];
+              };
+              nix.extraOptions = ''
+                experimental-features = nix-command flakes
+              '';
+              services.nix-daemon.enable = true;
+              system.stateVersion = 4;
+              system.defaults.NSGlobalDomain._HIHideMenuBar = false;
+              system.defaults.dock.autohide = true;
+            }
+          )
+          home-manager.darwinModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.ryanmorton.imports = [
+                ./modules/home-manager
+              ];
+            };
+          }
+        ];
+      };
+
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          config = {
+            allowUnfree = true;
+            allowUnfreePredicate = true;
+            allowBroken = true;
+          };
+        };
+        modules = [
+          ./modules/nixos-config
+          home-manager.nixosModules.home-manager
+          (
+            { pkgs, ... }:
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                backupFileExtension = "bak";
+                users.ryanm.wayland.windowManager.hyprland.plugins = [
+                  hypr-darkwindow.packages.${pkgs.system}.Hypr-DarkWindow
+                ];
+                users.ryanm.imports = [
+                  ./modules/home-manager
+                ];
+                extraSpecialArgs = {
+                  inherit inputs;
+                  system = "x86_64-linux";
+                };
+              };
+            }
+          )
+        ];
+      };
+
+      darwinConfigurations.Ryans-Mac-Mini = darwin.lib.darwinSystem {
+        system = "x86_64-darwin";
+        pkgs = import nixpkgs {
+          system = "x86_64-darwin";
+          config = {
+            allowUnfree = true;
+            allowUnfreePredicate = true;
+            allowBroken = true;
+          };
+        };
+        modules = [
+          (
+            { pkgs, ... }:
+            {
+              users.users.ryanmorton = {
+                uid = 501;
+                name = "ryanmorton";
+                home = "/Users/ryanmorton";
+                shell = pkgs.zsh;
+              };
+              programs.zsh.enable = true;
+              # homebrew.enable = true;
+              # homebrew.brews = [ ];
+              environment = {
+                shells = [
+                  pkgs.bash
+                  pkgs.zsh
+                ];
+                systemPackages = [
+                  pkgs.coreutils
+                  pkgs.git
+                  pkgs.curl
+                  pkgs.wget
+                  pkgs.zellij
+                  pkgs.jankyborders
+                ];
+                systemPath = [
+                  "~/.config/composer/vendor/bin"
+                ];
+              };
+              nix.extraOptions = ''
+                experimental-features = nix-command flakes
+              '';
+              system.stateVersion = 4;
+              system.defaults.NSGlobalDomain._HIHideMenuBar = false;
+              system.defaults.dock.autohide = true;
+            }
+          )
+          home-manager.darwinModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.ryanmorton.imports = [
+                ./modules/home-manager
+              ];
+            };
+          }
+        ];
+      };
+    };
 }
--- modules/nixos-config/default.nix
+++ modules/nixos-config/default.nix
@@ -2,151 +2,176 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, lib, ... }:
 {
-    imports =
-        [ # Include the results of the hardware scan.
-            ./hardware-configuration.nix
-        ];
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
-    boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.enable = true;
 
-    boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
-    boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
-    networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "nixos"; # Define your hostname.
 
-    # Enable networking
-    networking.networkmanager.enable = true;
-    services.tailscale.enable = true;
+  # Enable networking
+  networking.networkmanager.enable = true;
+  services.tailscale.enable = true;
 
-    # Configure network proxy if necessary
-    # networking.proxy.default = "http://user:password@proxy:port/";
-    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-    virtualisation.docker = {
-        enable = true;
-    };
+  virtualisation.docker = {
+    enable = true;
+  };
 
-    # Set your time zone.
-    time.timeZone = "America/Phoenix";
+  # Set your time zone.
+  time.timeZone = "America/Phoenix";
 
-    # Select internationalisation properties.
-    i18n.defaultLocale = "en_US.UTF-8";
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
 
-    i18n.extraLocaleSettings = {
-        LC_ADDRESS = "en_US.UTF-8";
-        LC_IDENTIFICATION = "en_US.UTF-8";
-        LC_MEASUREMENT = "en_US.UTF-8";
-        LC_MONETARY = "en_US.UTF-8";
-        LC_NAME = "en_US.UTF-8";
-        LC_NUMERIC = "en_US.UTF-8";
-        LC_PAPER = "en_US.UTF-8";
-        LC_TELEPHONE = "en_US.UTF-8";
-        LC_TIME = "en_US.UTF-8";
-    };
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
 
-    # Enable the X11 windowing system.
-    # You can disable this if you're only using the Wayland session.
-    services.xserver.enable = true;
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
 
-    # Enable the KDE Plasma Desktop Environment.
-    services.displayManager.sddm.enable = true;
-    # services.desktopManager.plasma6.enable = true;
-    programs.hyprland.enable = true;
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  # services.desktopManager.plasma6.enable = true;
 
-    # Configure keymap in X11
-    services.xserver.xkb = {
-        layout = "us";
-        variant = "";
-    };
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+    xwayland.enable = true;
+  };
 
-    # Enable CUPS to print documents.
-    services.printing.enable = true;
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
 
-    # Enable sound with pipewire.
-    services.pulseaudio.enable = false;
-    security.rtkit.enable = true;
-    services.pipewire = {
-        enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
-        pulse.enable = true;
-        # If you want to use JACK applications, uncomment this
-        #jack.enable = true;
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
 
-        # use the example session manager (no others are packaged yet so this is enabled by default,
-        # no need to redefine it in your config for now)
-        #media-session.enable = true;
-    };
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
 
-    # Enable touchpad support (enabled default in most desktopManager).
-    # services.xserver.libinput.enable = true;
-    programs.zsh.enable = true;
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+  programs.zsh.enable = true;
 
-    # Define a user account. Don't forget to set a password with ‘passwd’.
-    users.users.ryanm = {
-        isNormalUser = true;
-        description = "Ryan Morton";
-        extraGroups = [ "networkmanager" "wheel" "docker" ];
-        shell = pkgs.zsh;
-        packages = with pkgs; [
-            kdePackages.kate
-            sbctl
-            ghostty
-            os-prober
-            kitty
-            wofi
-            waybar
-            gh
-            hyprpaper
-        ];
-    };
-
-    # Enable automatic login for the user.
-    services.displayManager.autoLogin.enable = true;
-    services.displayManager.autoLogin.user = "ryanm";
-
-    # Install firefox.
-    programs.firefox.enable = true;
-
-
-    # List packages installed in system profile. To search, run:
-    # $ nix search wget
-    environment.systemPackages = with pkgs; [
-        wl-clipboard
-        docker-compose
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.ryanm = {
+    isNormalUser = true;
+    description = "Ryan Morton";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "docker"
     ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [
+      kdePackages.kate
+      sbctl
+      ghostty
+      os-prober
+      kitty
+      hyprland
+      wofi
+      waybar
+      gh
+      hyprpaper
+    ];
+  };
 
-    # Some programs need SUID wrappers, can be configured further or are
-    # started in user sessions.
-    # programs.mtr.enable = true;
-    # programs.gnupg.agent = {
-    #   enable = true;
-    #   enableSSHSupport = true;
-    # };
+  # Enable automatic login for the user.
+  # services.greetd = {
+  #   enable = true;
+  #   settings = rec {
+  #     initial_session = {
+  #       command = "${pkgs.hyprland}/bin/hyprland";
+  #       user = "ryanm";
+  #     };
+  #     default_session = initial_session;
+  #   };
+  # };
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "ryanm";
 
-    # List services that you want to enable:
+  # Install firefox.
+  programs.firefox.enable = true;
 
-    # Enable the OpenSSH daemon.
-    # services.openssh.enable = true;
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wl-clipboard
+    docker-compose
+  ];
 
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
-    # Open ports in the firewall.
-    # networking.firewall.allowedTCPPorts = [ ... ];
-    # networking.firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    # networking.firewall.enable = false;
+  # List services that you want to enable:
 
-    # This value determines the NixOS release from which the default
-    # settings for stateful data, like file locations and database versions
-    # on your system were taken. It‘s perfectly fine and recommended to leave
-    # this value at the release version of the first install of this system.
-    # Before changing this value read the documentation for this option
-    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-    system.stateVersion = "25.05"; # Did you read the comment?
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
 
 }
