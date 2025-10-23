37b8d6537fa5e1e5d4b1f7aab594ab4b935ab446
Ryan Morton <morton.ryan92@gmail.com>
2025-10-23 14:58:34 -0700

    Add cmus to waybar

--- modules/dots/waybar/nowplaying.sh
+++ modules/dots/waybar/nowplaying.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-player_status=$(playerctl --player=spotify status 2> /dev/null)
+player_status=$(playerctl --player=cmus,spotify status 2> /dev/null)
 if [ "$player_status" = "Playing" ]; then
-    echo "$(playerctl --player=spotify metadata artist) - $(playerctl --player=spotify metadata title)"
+    echo "$(playerctl --player=cmus,spotify metadata artist) - $(playerctl --player=cmus,spotify metadata title)"
 elif [ "$player_status" = "Paused" ]; then
-    echo " $(playerctl --player=spotify metadata artist) - $(playerctl --player=spotify metadata title)"
+    echo " $(playerctl --player=cmus,spotify metadata artist) - $(playerctl --player=cmus,spotify metadata title)"
 fi
