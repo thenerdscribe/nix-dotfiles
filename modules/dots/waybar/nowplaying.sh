7e8d0be5a970fcc5d93eb6dece7cfbbc57ea258c
Ryan Morton <morton.ryan92@gmail.com>
2025-10-30 13:18:17 -0700

    Add neo-pos1

--- modules/dots/waybar/nowplaying.sh
+++ modules/dots/waybar/nowplaying.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
-player_status=$(playerctl --player=cmus,spotify status 2> /dev/null)
-if [ "$player_status" = "Playing" ]; then
-    echo "$(playerctl --player=cmus,spotify metadata artist) - $(playerctl --player=cmus,spotify metadata title)"
-elif [ "$player_status" = "Paused" ]; then
-    echo " $(playerctl --player=cmus,spotify metadata artist) - $(playerctl --player=cmus,spotify metadata title)"
+player_status="$(playerctl --player=cmus,spotify status 2> /dev/null)";
+artist="$(playerctl --player=cmus,spotify metadata artist | sed 's/&/&amp;/g')";
+title=$(playerctl --player=cmus,spotify  metadata title | sed 's/&/&amp;/g');
+if [ ${#artist} -gt 10 ]; then
+    artist="$(echo $artist | cut -c 1-9)..."
 fi
+
+if [ "$player_status" = "Playing" ]; then
+    SYMBOL="";
+elif [ "$player_status" = "Paused" ]; then
+    SYMBOL="  ";
+fi
+
+echo "$SYMBOL$artist - $title";
--- modules/systems/nixos/configuration.nix
+++ modules/systems/nixos/configuration.nix
@@ -38,8 +38,8 @@
     54.71.90.183   listingmanager.walts.com
     54.71.90.183    phpadmin-production.walts.com
     34.216.166.84   phpadmin.walts.com php test-retail-inventory-api.walts.com test-retail-api-ospos.walts.com test-retail-ordermanager.walts.com test-ordermanager.walts.com test-inventory-api.walts.com test-api-ospos.walts.com test-listingmanager.walts.com
-    127.0.0.1 neo-tools.dev.walts.com awesome-ecomm.dev.walts.com inventory-api.dev.walts.com api-ospos.dev.walts.com gaming-schedule.test ordermanager.dev.walts.com forma-planner.test
-    50.112.66.233 test-www.walts.com
+    127.0.0.1 neo-tools.dev.walts.com awesome-ecomm.dev.walts.com inventory-api.dev.walts.com api-ospos.dev.walts.com gaming-schedule.test ordermanager.dev.walts.com forma-planner.test neo-pos1.dev.walts.com
+    50.112.66.233 test-www.walts.com 
     35.160.43.43 test-neo-pos1.walts.com test-retail-neo-pos1.walts.com
     35.87.153.218 test-neo-wpos2.walts.com test-retail-neo-wpos2.walts.com
     35.90.134.222           ae-staging.walts.com
@@ -211,6 +211,9 @@
     '';
     virtualHosts."http://forma-planner.test".extraConfig = ''
       reverse_proxy 127.0.0.1:8894
+    '';
+    virtualHosts."http://neo-pos1.dev.walts.com".extraConfig = ''
+      reverse_proxy 127.0.0.1:8895
     '';
   };
 
